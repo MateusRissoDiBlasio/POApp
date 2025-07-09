42e4d69265909721860d90404491f2a6f34f1950
unknown <mateusrisso@yahoo.com.br>
2025-07-09 16:02:45 -0300

    path correction

--- src/components/navigation/mobile/mobile-nav-bar-brand.js
+++ src/components/navigation/mobile/mobile-nav-bar-brand.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export const MobileNavBarBrand = ({ handleClick }) => {
   return (
     <div onClick={handleClick} className="mobile-nav-bar__brand">
-      <NavLink to="/POapp/PO">
+      <NavLink to="/PO">
         {/* <img
           className="mobile-nav-bar__logo"
           src="https://cdn.auth0.com/blog/hub/code-samples/hello-world/auth0-logo.svg"
